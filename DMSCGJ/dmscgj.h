4b73c729e6714367a8a41457fef3049becdcb753
HUA Hongwei <1185262830@qq.com>
2021-04-07 17:54:37 +0800

    add code

---
+++ DMSCGJ/dmscgj.h
@@ -0,0 +1,24 @@
+﻿#ifndef DMSCGJ_H
+#define DMSCGJ_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class DMSCGJ;
+}
+
+class DMSCGJ : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit DMSCGJ(QWidget *parent = 0);
+    ~DMSCGJ();
+
+private:
+    Ui::DMSCGJ *ui;
+
+    void initMenubar();
+};
+
+#endif // DMSCGJ_H
